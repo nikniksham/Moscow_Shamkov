61cc1f46bfe607cfbc1fbcaa5a5c7c9596c88270
nikniksham <nikniksham@gmail.com>
2019-11-15 00:06:44 +0300

    pygame 7 задача

--- PyGame.py
+++ PyGame.py
@@ -2,15 +2,25 @@ import pygame
 
 
 pygame.init()
-N = int(input('Введите размер ромбиков: '))
-width, height = int(input('Введите ширину поля: ')), int(input('Введите высоту поля: '))
+width, height = int(input('Введите ширину поля: ')), int(input('Введите высоту поля: '))  # Размеры поля
+Pass = 2  # Пропуски между кирпичами
+brick_size = [30, 15]  # Размеры кирпичей
+brick_color = (255, 0, 0)  # Цвет кирпичей
+#                                ВСЕ ЗНАЧЕНИЯ МОЖНО МЕНЯТЬ НА СВОЁ УСМОТРЕНИЕ!!!
+#                                Лично я советую: Pass = 4; brick_size = [45, 20]
 size = width, height
 screen = pygame.display.set_mode(size)
-screen.fill((255, 255, 0))
-for i in range(height // N):
-    for j in range(width // N):
-        pygame.draw.polygon(screen, (255, 125, 40), ((j * N + N // 2, i * N), ((j + 1) * N, i * N + N // 2),
-                                                (j * N + N // 2, (i + 1) * N), (j * N, i * N + N // 2)))
+screen.fill((255, 255, 255))
+y = 0
+for i in range(height // brick_size[1]):
+    x = 0
+    if i % 2 != 0:
+        x -= brick_size[0] // 2
+    for j in range(width // brick_size[0] + 1):
+        pygame.draw.rect(screen, brick_color, (j * brick_size[0] + x, i * brick_size[1] + y, brick_size[0],
+                                               brick_size[1]))
+        x += Pass
+    y += Pass
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
 pygame.quit()
