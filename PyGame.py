0a5387c0052b9f4607ae580b647a3084d349efda
nikniksham <nikniksham@gmail.com>
2019-11-14 18:52:16 +0300

    pygame 3 задача

--- PyGame.py
+++ PyGame.py
@@ -2,15 +2,20 @@ import pygame
 
 
 pygame.init()
-width, height = int(input()), int(input())
+size_window, col = int(input()), int(input())
 # width, height = 500, 500
-size = width, height
+size = size_window, size_window
+size_ = size_window // col
 screen = pygame.display.set_mode(size)
-screen.fill((255, 0, 0))
-pygame.draw.line(screen, (0, 0, 0), (0, 0), (width, 0))
-pygame.draw.line(screen, (0, 0, 0), (width - 1, 1), (width - 1, height - 1))
-pygame.draw.line(screen, (0, 0, 0), (width - 1, height - 1), (1, height - 1))
-pygame.draw.line(screen, (0, 0, 0), (0, height), (0, 0))
+screen.fill((0, 0, 0))
+for i in range(col):
+    for j in range(col):
+        if i % 2 != 0 and j % 2 == 0:
+            print(i, j, i % 2, j % 2, 1)
+            pygame.draw.rect(screen, (255, 255, 255), (i * size_, j * size_, size_, size_))
+        if i % 2 == 0 and j % 2 != 0:
+            print(i, j, i % 2, j % 2, 2)
+            pygame.draw.rect(screen, (255, 255, 255), (i * size_, j * size_, size_, size_))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
