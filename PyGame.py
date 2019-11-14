240780260e86aa25d5c7494b0671d5998c215765
nikniksham <nikniksham@gmail.com>
2019-11-14 19:19:34 +0300

    pygame 4 задача

--- PyGame.py
+++ PyGame.py
@@ -3,19 +3,19 @@ import pygame
 
 pygame.init()
 vvod = input().split()
-size_window, col = int(vvod[0]), int(vvod[1])
+thickness, col = int(vvod[0]), int(vvod[1])
 # width, height = 500, 500
-size = size_window, size_window
-size_ = size_window // col
+size = col * thickness * 2, col * thickness * 2
 screen = pygame.display.set_mode(size)
 screen.fill((0, 0, 0))
+RGB = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
+j = 0
 for i in range(col):
-    for j in range(col):
-        if i % 2 != 0 and j % 2 == 0:
-            pygame.draw.rect(screen, (255, 255, 255), (i * size_, j * size_, size_, size_))
-        if i % 2 == 0 and j % 2 != 0:
-            pygame.draw.rect(screen, (255, 255, 255), (i * size_, j * size_, size_, size_))
-pygame.display.flip()
+    pygame.draw.circle(screen, RGB[j], ((col * thickness), (col * thickness)), thickness * (col - i))
+    j += 1
+    if j == len(RGB):
+        j = 0
+    pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
 pygame.quit()
