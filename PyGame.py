c27dca53959beb01a4eb28a45a19050afb6e4305
nikniksham <nikniksham@gmail.com>
2019-11-14 18:16:10 +0300

    pygame 2 задача

--- PyGame.py
+++ PyGame.py
@@ -3,10 +3,14 @@ import pygame
 
 pygame.init()
 width, height = int(input()), int(input())
+# width, height = 500, 500
 size = width, height
 screen = pygame.display.set_mode(size)
-pygame.draw.line(screen, (255, 255, 255), (0, 0), (width, height), 5)
-pygame.draw.line(screen, (255, 255, 255), (width, 0), (0, height), 5)
+screen.fill((255, 0, 0))
+pygame.draw.line(screen, (0, 0, 0), (0, 0), (width, 0))
+pygame.draw.line(screen, (0, 0, 0), (width - 1, 1), (width - 1, height - 1))
+pygame.draw.line(screen, (0, 0, 0), (width - 1, height - 1), (1, height - 1))
+pygame.draw.line(screen, (0, 0, 0), (0, height), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
