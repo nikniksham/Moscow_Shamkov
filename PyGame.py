6c2c75e01fe9e1cf88e01c49674052ca61c67660
nikniksham <nikniksham@gmail.com>
2019-11-14 23:41:45 +0300

    pygame 6 задача

--- PyGame.py
+++ PyGame.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode(size)
 screen.fill((255, 255, 0))
 for i in range(height // N):
     for j in range(width // N):
-        pygame.draw.polygon(screen, (0, 0, 0), ((j * N + N // 2, i * N), ((j + 1) * N, i * N + N // 2),
+        pygame.draw.polygon(screen, (255, 125, 40), ((j * N + N // 2, i * N), ((j + 1) * N, i * N + N // 2),
                                                 (j * N + N // 2, (i + 1) * N), (j * N, i * N + N // 2)))
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
