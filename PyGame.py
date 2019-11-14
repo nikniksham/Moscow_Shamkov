7e126b965a4e651ff4330e9f47d3e4eba7ef28df
nikniksham <nikniksham@gmail.com>
2019-11-14 23:39:34 +0300

    pygame 6 задача

--- PyGame.py
+++ PyGame.py
@@ -2,16 +2,15 @@ import pygame
 
 
 pygame.init()
-N = int(input())
-# N = 1
-width, height = 300, 300
+N = int(input('Введите размер ромбиков: '))
+width, height = int(input('Введите ширину поля: ')), int(input('Введите высоту поля: '))
 size = width, height
 screen = pygame.display.set_mode(size)
-screen.fill((0, 0, 0))
-pi = 3.14
-for i in range(N):
-    pygame.draw.ellipse(screen, (255, 255, 255), (0, 150 - ((150 // N) * (i + 1)), 300, (300 // N) * (i + 1)), 1)
-    pygame.draw.ellipse(screen, (255, 255, 255), (150 - ((150 // N) * (i + 1)), 0, (300 // N) * (i + 1), 300), 1)
+screen.fill((255, 255, 0))
+for i in range(height // N):
+    for j in range(width // N):
+        pygame.draw.polygon(screen, (0, 0, 0), ((j * N + N // 2, i * N), ((j + 1) * N, i * N + N // 2),
+                                                (j * N + N // 2, (i + 1) * N), (j * N, i * N + N // 2)))
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
 pygame.quit()
