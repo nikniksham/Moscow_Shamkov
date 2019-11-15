9324fb4d5463570798a9a4fa561ce52946050047
nikniksham <nikniksham@gmail.com>
2019-11-15 08:11:48 +0300

    pygame 8 задача

--- PyGame.py
+++ PyGame.py
@@ -2,25 +2,26 @@ import pygame
 
 
 pygame.init()
-width, height = int(input('Введите ширину поля: ')), int(input('Введите высоту поля: '))  # Размеры поля
-Pass = 2  # Пропуски между кирпичами
-brick_size = [30, 15]  # Размеры кирпичей
-brick_color = (255, 0, 0)  # Цвет кирпичей
-#                                ВСЕ ЗНАЧЕНИЯ МОЖНО МЕНЯТЬ НА СВОЁ УСМОТРЕНИЕ!!!
-#                                Лично я советую: Pass = 4; brick_size = [45, 20]
+input_ = input().split()
+W, Hue = int(input_[0]), int(input_[1])
+if Hue > 360:
+    Hue = 360
+if Hue <= 0:
+    Hue = 1
+color = pygame.Color(100, Hue, 100)
+color2 = pygame.Color(75, Hue, 100)
+color3 = pygame.Color(50, Hue, 100)
+hsv = color.hsva
+color.hsva = (hsv[0], hsv[1], hsv[2], hsv[3])
+# W = 300
+width, height = 300, 300
 size = width, height
 screen = pygame.display.set_mode(size)
-screen.fill((255, 255, 255))
-y = 0
-for i in range(height // brick_size[1]):
-    x = 0
-    if i % 2 != 0:
-        x -= brick_size[0] // 2
-    for j in range(width // brick_size[0] + 1):
-        pygame.draw.rect(screen, brick_color, (j * brick_size[0] + x, i * brick_size[1] + y, brick_size[0],
-                                               brick_size[1]))
-        x += Pass
-    y += Pass
+pygame.draw.rect(screen, color2, (80, 160, W, W))
+pygame.draw.polygon(screen, color, ((80, 160), (80 + W // 2, 160 - W // 2),
+                                          (80 + W + W // 2, 160 - W // 2), (80 + W, 160)))
+pygame.draw.polygon(screen, color3, ((80 + W + W // 2, 160 - W // 2), (80 + W, 160),
+                                          (80 + W, 160 + W), (80 + W // 2 + W, 160 + W // 2)))
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.update()
 pygame.quit()
