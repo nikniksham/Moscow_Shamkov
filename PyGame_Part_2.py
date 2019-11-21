a6f4e103441cf0ec3191fce567368656e52ee806
nikniksham <nikniksham@gmail.com>
2019-11-21 12:18:36 +0300

    pygame_2 задача 6

--- PyGame_Part_2.py
+++ PyGame_Part_2.py
@@ -1,12 +1,11 @@
 import pygame
 
 pygame.init()
-width, height = 300, 300
-mouse_pos = [0, 0]
-rect_pos = [0, 0]  # Координаты квадрата
-size = (width, height)
+size = [width, height] = [501, 501]
+x, y = width // 2, height // 2
 running = True
 f_click = False
+mouse_pos = [width // 2, height // 2]
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 while running:
@@ -14,17 +13,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEMOTION:
-            click = pygame.mouse.get_pressed()
-            mouse = pygame.mouse.get_pos()
-            if click[0] == 1 and rect_pos[0] <= mouse[0] <= rect_pos[0] + 100 and \
-                    rect_pos[1] <= mouse[1] <= rect_pos[1] + 100 and f_click is False:
-                f_click = True
-                mouse_pos = [mouse[0] - rect_pos[0], mouse[1] - rect_pos[1]]
-            if click[0] == 0 and f_click:
-                f_click = False
-            if f_click:
-                rect_pos[0], rect_pos[1] = mouse[0] - mouse_pos[0], mouse[1] - mouse_pos[1]
-    pygame.draw.rect(screen, (0, 255, 0), (rect_pos[0], rect_pos[1], 100, 100))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+    if x < mouse_pos[0]:
+        x += 1
+    if x > mouse_pos[0]:
+        x -= 1
+    if y < mouse_pos[1]:
+        y += 1
+    if y > mouse_pos[1]:
+        y -= 1
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 20)
     pygame.display.flip()
     clock.tick(60)
