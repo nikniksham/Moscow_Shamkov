eab05cc84b4c314d89a12fdfb37adb056ea9135b
nikniksham <nikniksham@gmail.com>
2019-11-21 12:05:47 +0300

    pygame_2 задача 4

--- PyGame_Part_2.py
+++ PyGame_Part_2.py
@@ -1,35 +1,30 @@
 import pygame
 
 pygame.init()
-width, height = 1280, 720
-speed = 3  # Скорость шариков
+width, height = 300, 300
+mouse_pos = [0, 0]
+rect_pos = [0, 0]  # Координаты квадрата
 size = (width, height)
 running = True
+f_click = False
 screen = pygame.display.set_mode(size)
-c_l = []  # Список с кругами (circle_list)
 clock = pygame.time.Clock()
 while running:
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                pos_x, pos_y = pygame.mouse.get_pos()
-                c_l.append([[-speed, -speed], [pos_x, pos_y]])
-    for c in range(len(c_l)):
-        x, y = c_l[c][1]
-        motion_x, motion_y = c_l[c][0]
-        if x + motion_x > width - 10 or x + motion_x < 0 + 10:
-            motion_x *= -1
-        if y + motion_y > height - 10 or y + motion_y < 0 + 10:
-            motion_y *= -1
-        x += motion_x
-        y += motion_y
-        c_l[c][1] = [x, y]
-        c_l[c][0] = [motion_x, motion_y]
-
-    for c in range(len(c_l)):
-        pygame.draw.circle(screen, (255, 255, 255), (c_l[c][1]), 10)
+        if event.type == pygame.MOUSEMOTION:
+            click = pygame.mouse.get_pressed()
+            mouse = pygame.mouse.get_pos()
+            if click[0] == 1 and rect_pos[0] <= mouse[0] <= rect_pos[0] + 100 and \
+                    rect_pos[1] <= mouse[1] <= rect_pos[1] + 100 and f_click is False:
+                f_click = True
+                mouse_pos = [mouse[0] - rect_pos[0], mouse[1] - rect_pos[1]]
+            if click[0] == 0 and f_click:
+                f_click = False
+            if f_click:
+                rect_pos[0], rect_pos[1] = mouse[0] - mouse_pos[0], mouse[1] - mouse_pos[1]
+    pygame.draw.rect(screen, (0, 255, 0), (rect_pos[0], rect_pos[1], 100, 100))
     pygame.display.flip()
     clock.tick(60)
