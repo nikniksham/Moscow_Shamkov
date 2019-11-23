70d189cf9d89a2f799e853ed2534f3f48715535f
nikniksham <nikniksham@gmail.com>
2019-11-23 14:22:48 +0300

    pygame_2 задача 7

--- PyGame_Part_2.py
+++ PyGame_Part_2.py
@@ -1,42 +1,35 @@
 import pygame
 
 pygame.init()
-coords = []
-zoom = 10
-center = [300, 300]
-point_list = []
-for elem in input().split(', '):
-    a, b = elem.split(';')[0][1:], elem.split(';')[1][:-1]
-    if ',' in a:
-        a = a[:a.find(',')]
-    if ',' in b:
-        b = b[:b.find(',')]
-    point_list.append([round(float(a)), round(float(b)) * -1])
-    coords.append([round(float(a)), round(float(b)) * -1])
+rect_list = []
 size = [width, height] = [600, 600]
 running = True
-f_zoom = True
+f_motion = False
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 while running:
     screen.fill((0, 0, 0))
-    if f_zoom:
-        f_zoom = False
-        for i in range(len(coords)):
-            elem = coords[i]
-            elem = [elem[0] * zoom + center[0], elem[1] * zoom + center[1]]
-            point_list[i] = elem
+    for elem in rect_list:
+        pygame.draw.rect(screen, (255, 255, 255), elem, 5)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 4:
-                zoom += 1
-                f_zoom = True
-            if event.button == 5:
-                if zoom > 1:
-                    zoom -= 1
-                f_zoom = True
-    pygame.draw.polygon(screen, (255, 255, 255), point_list, 1)
+            if event.button == 1:
+                f_motion = True
+                mouse = pygame.mouse.get_pos()
+                rect_list.append([mouse[0], mouse[1], 0, 0])
+        if event.type == pygame.MOUSEMOTION and f_motion:
+            if pygame.mouse.get_pressed()[0] == 1:
+                mouse = pygame.mouse.get_pos()
+                rect = rect_list[-1]
+                x, y = mouse[0] - rect[0], mouse[1] - rect[1]
+                rect_list[-1] = [rect[0], rect[1], x, y]
+            else:
+                f_motion = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_z and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    if len(rect_list) > 0:
+                        rect_list = rect_list[:-1]
     pygame.display.flip()
     clock.tick(60)
