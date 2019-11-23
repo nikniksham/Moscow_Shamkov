37b0eb7ff80da6784d27dae27cc0f18eb4bc462d
nikniksham <nikniksham@gmail.com>
2019-11-23 13:31:02 +0300

    pygame_2 задача 8

--- PyGame_Part_2.py
+++ PyGame_Part_2.py
@@ -1,20 +1,42 @@
 import pygame
 
 pygame.init()
-size = [width, height] = [200, 200]
+coords = []
+zoom = 10
+center = [300, 300]
+point_list = []
+for elem in input().split(', '):
+    a, b = elem.split(';')[0][1:], elem.split(';')[1][:-1]
+    if ',' in a:
+        a = a[:a.find(',')]
+    if ',' in b:
+        b = b[:b.find(',')]
+    point_list.append([round(float(a)), round(float(b)) * -1])
+    coords.append([round(float(a)), round(float(b)) * -1])
+size = [width, height] = [600, 600]
 running = True
-number = 0
-font = pygame.font.Font(None, 150)
+f_zoom = True
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 while running:
     screen.fill((0, 0, 0))
-    text = font.render(str(number), 1, (255, 0, 0))
+    if f_zoom:
+        f_zoom = False
+        for i in range(len(coords)):
+            elem = coords[i]
+            elem = [elem[0] * zoom + center[0], elem[1] * zoom + center[1]]
+            point_list[i] = elem
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == 17:
-            number += 1
-    screen.blit(text, (width // 2 - text.get_width() // 2, height // 2 - text.get_height() // 2))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 4:
+                zoom += 1
+                f_zoom = True
+            if event.button == 5:
+                if zoom > 1:
+                    zoom -= 1
+                f_zoom = True
+    pygame.draw.polygon(screen, (255, 255, 255), point_list, 1)
     pygame.display.flip()
     clock.tick(60)
--- first.py
+++ first.py
@@ -1,1 +1,8 @@
-print('My first git-repo')
+coords = []
+for elem in input().split(', '):
+    a, b = elem.split(';')[0][1:], elem.split(';')[1][:-1]
+    if ',' in a:
+        a = a[:a.find(',')]
+    if ',' in b:
+        b = b[:b.find(',')]
+    coords.append([int(a), int(b)])
